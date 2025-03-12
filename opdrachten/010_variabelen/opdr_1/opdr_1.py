6d3618de3be0060b0c9c504586df45b440339fd6
saitamapunch <Jarno@patrijs.nl>
2025-03-12 15:21:12 +0100

    message

---
+++ opdrachten/010_variabelen/opdr_1/opdr_1.py
@@ -0,0 +1,8 @@
+# Opdracht 1
+# Naam student:
+# Groep:
+
+# Hier komt je code...
+
+my_list = [1, 3, "tekst", 4.86]
+print(my_list)
---
+++ opdrachten/010_variabelen/opdr_2/opdr_2.py
@@ -0,0 +1,9 @@
+# Opdracht 1
+# Naam student:
+# Groep:
+
+#
+
+my_tuple = ( "lekker weertje")
+print(my_tuple)
+
