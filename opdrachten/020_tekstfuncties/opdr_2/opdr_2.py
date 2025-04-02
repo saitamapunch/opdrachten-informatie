db13529443cd222dbc6f0bcdb8e21225a82fd7d0
saitamapunch <Jarno@patrijs.nl>
2025-04-02 14:44:04 +0200

    klaar

--- opdrachten/020_tekstfuncties/opdr_2/opdr_2.py
+++ opdrachten/020_tekstfuncties/opdr_2/opdr_2.py
@@ -2,8 +2,13 @@
 # Naam student:
 # Groep:
 
-# Hier komt je code...
+# Hier komt je co
+my_string = "Tinus gaat op zijn tandem naar de hottentottententoonstelling"
 
-my_list = ...
+# Tel hoe vaak de letter "t" voorkomt
+aantal_t = my_string.count("t")
+
+# Print het resultaat op het scherm
+print(f'De letter "t" komt {aantal_t}x voor in my_string')
 
 print()
