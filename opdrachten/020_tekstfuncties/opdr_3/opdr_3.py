f1fa5e82b068e866f152c6cf909ce1cd9157d05d
saitamapunch <Jarno@patrijs.nl>
2025-04-02 14:50:32 +0200

    klaar

--- opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
+++ opdrachten/020_tekstfuncties/opdr_3/opdr_3.py
@@ -3,3 +3,18 @@
 # Groep:
 
 # Hier komt je code...
+# Definitie van de losse regels van één kerstboom
+boom = [
+    "    *    ",
+    "   ***   ",
+    "  *****  ",
+    " ******* ",
+    "*********",
+    "   ***   ",
+    "   ***   ",
+    "   ***   "
+]
+
+# Teken vijf bomen naast elkaar
+for regel in boom:
+    print((regel + '  ') * 5)
